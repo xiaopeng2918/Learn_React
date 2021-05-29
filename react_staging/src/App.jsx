e74f1b7836deea05cf463e43b2f97b40520fa965
yxp2918 <2918369878@qq.com>
2021-05-29 08:37:26 +0800

    修改App处理addtodo事件，只写属于组件的逻辑。其他逻辑交给子组件

--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -16,27 +16,13 @@ export default class App extends Component {
       { id: '3', name: '学习', done: false },
     ]
   }
-  // 定义一个函数，这个函数用来修改当前组件todos状态 并将这个函数传给Header子组件
-  addtodo = (event) => {
+  // 定义一个函数，这个函数用来为todos添加一个新的todo 并将这个函数传给Header子组件
+  addtodo = (todoName) => {
     // 获取到当前组件todos状态
     const { todos } = this.state;
-    // 获取到事件子组件input输入框的值
-    // 由于是在input输入框上回调这个函数并拿取这个input输入框的值 所以这里不需要使用ref 用事件对象
-    // keyCode为js事件对象的一个源生属性，获取触发事件对象按下键盘 键 对应的值 这里回车为13
-    const { target, keyCode } = event;
     // 包装一个todoObj
-    const todoObj = { id: nanoid(), name: target.value, done: false };
-    if (keyCode === 13) {
-      // 判断一下，如果输入为空，不进行添加操作
-      if (target.value.trim() === '') {
-        alert('输入的值不能为空');
-        target.value = '';
-        return
-      }
-      this.setState({ todos: [todoObj, ...todos] });
-      // 清空输入框
-      target.value = '';
-    }
+    const todoObj = { id: nanoid(), name: todoName, done: false };
+    this.setState({ todos: [todoObj, ...todos] });
   }
   render() {
     // 获取状态数据传给子组件List
--- react_staging/src/Components/Header/index.jsx
+++ react_staging/src/Components/Header/index.jsx
@@ -3,12 +3,32 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Header extends Component {
-  render() {
+  handleKeyUp = (event) => {
+    // 获取父组件传来的addtodo
     const { addtodo } = this.props;
+    // 获取到事件子组件input输入框的值
+    // 由于是在input输入框上回调这个函数并拿取这个input输入框的值 所以这里不需要使用ref 用事件对象
+    // keyCode为js事件对象的一个源生属性，获取触发事件对象按下键盘 键 对应的值 这里回车为13
+    const { target, keyCode } = event;
+    if (keyCode === 13) {
+      // 判断一下，如果输入为空，不进行添加操作
+      if (target.value.trim() === '') {
+        alert('输入的值不能为空');
+        // 清空是输入的空格
+        target.value = '';
+        return
+      }
+      // 执行父子间传来的函数，添加todo
+      addtodo(target.value);
+      // 清空输入框的值
+      target.value = '';
+    }
+  }
+  render() {
 
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" onKeyUp={addtodo}/>
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认" onKeyUp={handleKeyUp} />
       </div>
     )
   }
